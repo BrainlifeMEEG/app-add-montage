75f8f6805714aad1d544a640bd657325e65403be
Kami Salibayeva <61473793+KSalibay@users.noreply.github.com>
2023-04-28 22:28:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ cap_montage = mne.channels.make_standard_montage(montage)
 
 if len(config['rename_channels']) >= 1:
     rename_channels = config['rename_channels']
-    rename_ch = dict((x.strip(), int(y.strip())) 
+    rename_ch = dict((x.strip(), y.strip()) 
                      for x, y in (element.split('-')
                                   for element in rename_channels.split(',')))
     cap_montage.rename_channels(rename_ch)
